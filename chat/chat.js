956fc6585077ed327197c4edc7bc70653d32d819
Nabee-Rizwan-Quadri <80901728+Nabee-Rizwan-Quadri@users.noreply.github.com>
2021-07-10 19:31:22 +0500

    almost final

--- chat/chat.js
+++ chat/chat.js
@@ -1,37 +1,84 @@
-var db = firebase.firestore();
-// const userId = firebase.auth().currentUser.user.uid;
 let message, userId, userAvatar, userName
 
 firebase.auth().onAuthStateChanged((user) => {
-    if (!user) {
-        // User is signed in, see docs for a list of available properties
-        // https://firebase.google.com/docs/reference/js/firebase.User
-        location.href = '../auth-screens/login/login.html'
-            // ...
-    } else {
-  
-        userId = user.
-        userAvatar = 
-        userName =
-
-        }).catch((error) => {
-            console.log("Error getting document:", error);
-        });
-        // User is signed out
-        // ...
-    }
-});
+        if (user) {
+            console.log(user)
+            userId = user.uid
+            userAvatar = user.photoURL
+            userName = user.displayName
+            console.log(userName)
+        } else {
+            location.href = '../auth-screens/login/login.html'        }
+      });
 
 function submit(){
     let message = document.getElementById("userInput").value
+
     let messageInfo = {
-        message,
         userId,
+        userName,
         userAvatar,
-        userName
+        message,
+        // createdOn = firebase.firestore.FieldValue.serverTimestamp().
     }
 
+    // console.log(messageInfo)
+
+    firebase.firestore().collection("Chats").add({messageInfo})
+    .then((docRef) => {
+        console.log("Document written with ID: ", docRef.id);
+    })
+    .catch((error) => {
+        console.error("Error adding document: ", error);
+    });
+
+    }
+
+let displayData = () => {
+
+    firebase.firestore().collection("Chats")
+    .get()
+    .then((querySnapshot) => {
+        
+        querySnapshot.forEach((doc) => {
+            let htmldiv = ``
+            // doc.data() is never undefined for query doc snapshots
+
+            user = doc.data().messageInfo
+
+            console.log(user)
+
+            let position = userId == user.uid ? 'left': 'right'
+            
+            let status = userId == user.uid ? 'status offline': 'status online'
+            
+            let now = new Date()
+            let time = now.toDateString()
+
+            
+            
+            htmldiv += `<div class="answer ${position}">
+                <div class="avatar">
+                  <img src="${user.userAvatar}" alt="${user.userName}">
+                  <div class="${status}"></div>
+                </div>
+                <div class="name">${user.userName}</div>
+                <div class="text">
+                  ${user.message}
+                </div>
+                <div class="time">${time}</div>
+              </div>`;
+            
+            document.getElementById('chat-messages').innerHTML = htmldiv
+        });
+    })
+    .catch((error) => {
+        console.log("Error getting documents: ", error);
+    });
+
 }
+    
+displayData()
 
 let logout = () => {
     firebase.auth().signOut().then(() => {
@@ -41,7 +88,3 @@ let logout = () => {
         // An error happened.
     });
 }
-
-//firebase username on dashboard
-// console.log(userName)
-// document.getElementById('userName').innerText = `Welcome ${userName}!`
